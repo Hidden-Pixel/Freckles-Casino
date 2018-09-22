81e714507ddc4a9cf917409519c6c46816a6bb44
Nick Kane <nkanedev@gmail.com>
2018-09-21 23:29:48 -0400

    renaming textures and setting up initial texture loading of a basic scene

--- game/src/main.c
+++ game/src/main.c
@@ -17,9 +17,14 @@ global_variable char *GlobalWindowTitle = "Mr. Freckles Casino";
 global_variable unsigned char GlobalRunning = 1;
 global_variable int GlobalTargetFPS = 60;
 
-global_variable Texture2D TestTexture1;
-global_variable int X1 = WIDTH / 2;
-global_variable int Y1 = HEIGHT / 2;
+global_variable Texture2D BlankGreenTableTexture;
+global_variable Vector2 TableVector2;
+
+global_variable Texture2D CardSlotTexture;
+global_variable Vector2 CardSlotVector2[9];
+
+global_variable Texture2D BackOfCardTexture;
+global_variable Vector2 CardVector2[9];
 
 void
 LoadTextures();
@@ -63,22 +68,44 @@ main(void)
 void
 LoadTextures()
 {
-    Image tempImage = LoadImage("assets/textures/Cards/BackOfCard/BackOfCard.png");
-    ImageResizeNN(&tempImage, tempImage.width * 10 , tempImage.height * 10);
-    TestTexture1 = LoadTextureFromImage(tempImage);
+    Image tempImage = LoadImage("assets/textures/Background/BlankTableGreen.png");
+    ImageResizeNN(&tempImage, GlobalWindowWidth, GlobalWindowHeight / 2);
+    BlankGreenTableTexture = LoadTextureFromImage(tempImage);
+    UnloadImage(tempImage);
+    TableVector2.y = (GlobalWindowHeight - BlankGreenTableTexture.height);
+    TableVector2.x = 0;
+
+    int resizedWidth = ((BlankGreenTableTexture.width - 10) / 9);
+    resizedWidth = (int)floor(((float)resizedWidth) * 0.60f);
+    int resizedHeight = ((BlankGreenTableTexture.height - 30) / 2);
+    resizedHeight = (int)floor(((float)resizedHeight) * 0.75f);
+
+    tempImage = LoadImage("assets/textures/Background/CardSlot.png");
+    ImageResizeNN(&tempImage, resizedWidth, resizedHeight);
+    CardSlotTexture = LoadTextureFromImage(tempImage);
+    UnloadImage(tempImage);
+
+    resizedWidth = (int)floor((float)resizedWidth * 0.75f);
+    resizedHeight = (int)floor((float)resizedHeight * 0.85f);
+
+    tempImage = LoadImage("assets/textures/Cards/BackOfCard/BackOfCard.png");
+    ImageResizeNN(&tempImage, resizedWidth, resizedHeight);
+    BackOfCardTexture = LoadTextureFromImage(tempImage);
     UnloadImage(tempImage);
 }
 
 void
 UnloadTextures()
 {
-    UnloadTexture(TestTexture1);
+    UnloadTexture(BlankGreenTableTexture);
+    UnloadTexture(CardSlotTexture);
+    UnloadTexture(BackOfCardTexture);
 }
 
 void
 ProcessInput()
 {
-    // TODO(martin): add support for mouse click on cards
+    // TODO(martin): add support for inputs
 }
 
 void
@@ -86,8 +113,10 @@ Render()
 {
     BeginDrawing();
     {
-        ClearBackground(GREEN);
-        DrawTexture(TestTexture1, X1, Y1, WHITE);
+        ClearBackground(BLANK);
+        DrawTexture(BlankGreenTableTexture, TableVector2.x, TableVector2.y, WHITE);
+        DrawTexture(CardSlotTexture, TableVector2.x, TableVector2.y + 25, WHITE);
+        DrawTexture(BackOfCardTexture, TableVector2.x + 10, TableVector2.y + 35, WHITE);
     }
     EndDrawing();
 }

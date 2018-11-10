6906d5786fceeb8f5e17d2fc3688a9cb1051a304
Nick Kane <nkanedev@gmail.com>
2018-11-10 12:35:18 -0500

    initial setup for sound meta data

--- game/src/include/FC/sound.h
+++ game/src/include/FC/sound.h
@@ -5,6 +5,17 @@
 
 #include "raylib.h"
 
+typedef struct _soundMeta
+{
+    unsigned int    totalPlays;
+    unsigned int    playLimit;
+    float           entirePlayLength;
+    float           lastPlayTime;
+} SoundMeta;
+
+SoundMeta
+CreateSoundMeta(Music *sound);
+
 void
 AddSoundToBuffer(Music *Sound);
 
--- game/src/sound.c
+++ game/src/sound.c
@@ -6,13 +6,32 @@
 #include <FC/sound.h>
 #include <FC/defines.h>
 
+#define SOUND_BUFFER_MAX 10
+
 internal unsigned int SoundBufferSize = 0;
-internal Music *SoundBuffer[10];
+internal Music *SoundBuffer[SOUND_BUFFER_MAX];
+
+SoundMeta
+CreateSoundMeta(Music *sound)
+{
+    SoundMeta s = (SoundMeta)
+    {
+        .totalPlays         = 0,
+        .playLimit          = 0,
+        .entirePlayLength   = GetMusicTimeLength(*sound),
+        .lastPlayTime       = 0,
+    };
+    return s;
+}
 
 void
 AddSoundToBuffer(Music *sound)
 {
     // TODO(nick): handle overflow of add request by removing first
+    if (len(SoundBuffer) >= SOUND_BUFFER_MAX)
+    {
+        RemoveSoundFromBuffer();
+    }
     SoundBuffer[SoundBufferSize] = sound;
     SoundBufferSize++;
 }
@@ -21,12 +40,12 @@ void
 RemoveSoundFromBuffer()
 {
     // TODO(nick):
-    // NOTE: for now, always remove from the front
 }
 
 void
 PlaySounds()
 {
+    // TODO(nick): add meta data checking here ...
     for (int i = 0; i < len(SoundBuffer); i++)
     {
         if (SoundBuffer[i] != NULL)
@@ -39,6 +58,7 @@ PlaySounds()
 void
 UpdateSounds()
 {
+    // TODO(nick): add meta data checking here ...
     for (int i = 0; i < len(SoundBuffer); i++)
     {
         if (SoundBuffer[i] != NULL)
